47873027634116690775b84ac3630b38d0d59e0c
magnetarz <kwardrip105@gmail.com>
2022-07-15 17:17:37 -0500

    On branch master created player centered camera and hitbox for proper proper sprite layering.

--- game/level/code/level.py
+++ game/level/code/level.py
@@ -30,7 +30,7 @@ class Level:
 
     def run(self):
         #update and draw the game
-        self.visible_sprites.draw(self.display_surface)
+        self.visible_sprites.custom_draw(self.player)
         self.visible_sprites.update()
         
 
@@ -40,10 +40,17 @@ class YSortCameraGroup(pygame.sprite.Group):
         #general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
-        self.offset = pygame.math.Vector2(100,200)
+        self.half_width = self.display_surface.get_size()[0] // 2
+        self.half_height = self.display_surface.get_size()[1] //2
+        self.offset = pygame.math.Vector2()
 
-    def custom_draw(self):
-        for sprite in self.sprites():
-            offset_pos = sprite.rect.topleft + self.offset
+    def custom_draw(self, player):
+
+        #getting the offset
+        self.offset.x = player.rect.centerx - self.half_width
+        self.offset.y = player.rect.centery - self.half_height
+        
+        # for sprite in self.sprites():
+        for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
+            offset_pos = sprite.rect.topleft - self.offset
             self.display_surface.blit(sprite.image, offset_pos)
-
