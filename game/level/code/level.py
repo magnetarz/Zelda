a6ebc05a83415e2540c3c964a29708143ae2e151
magnetarz <kwardrip105@gmail.com>
2022-07-17 22:57:17 -0500

    On branch master imported most of the games graphics and implemented  some of the sprite collision. Need to fix large objects bug and  get sprites in the right location from csv file.

--- game/level/code/level.py
+++ game/level/code/level.py
@@ -2,7 +2,8 @@ import pygame
 from settings import *
 from tile import Tile
 from player import Player
-
+from support import *
+from random import choice
 
 
 
@@ -19,14 +20,35 @@ class Level:
         self.create_map()
 
     def create_map(self):
-        for row_index, row in enumerate(WORLD_MAP):
-            for col_index, col in enumerate(row):
-                x = col_index * TILESIZE
-                y = row_index * TILESIZE
-                if col == 'x':
-                    Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
-                if col == 'p':
-                    self.player = Player((x,y),[self.visible_sprites],self.obstacle_sprites)
+        layouts = {
+            'boundary': import_csv_layout('/home/magnetar/Documents/coding/fun/python/Zelda/game/level/map/map_FloorBlocks.csv'),
+            'grass': import_csv_layout('/home/magnetar/Documents/coding/fun/python/Zelda/game/level/map/map_Grass.csv'),
+            'object': import_csv_layout('/home/magnetar/Documents/coding/fun/python/Zelda/game/level/map/map_Objects.csv')
+        }
+
+        graphics = {
+            'grass': import_folder('/home/magnetar/Documents/coding/fun/python/Zelda/game/level/graphics/grass'),
+            'objects': import_folder('/home/magnetar/Documents/coding/fun/python/Zelda/game/level/graphics/objects')
+        }
+        for style, layout in layouts.items():
+            for row_index, row in enumerate(layout):
+                for col_index, col in enumerate(row):
+                    if col != '-1':
+                        x = col_index * TILESIZE
+                        y = row_index * TILESIZE
+                        if style == 'boundary':
+                            Tile((x,y),[self.obstacle_sprites], 'invisible')
+                        if style == 'grass':
+                           random_grass_image = choice(graphics['grass'])
+                           Tile((x,y),[self.visible_sprites,self.obstacle_sprites],'grass', random_grass_image)
+                        if style == 'object':
+                            surf = graphics['objects'][int(col)]
+                            Tile((x,y), [self.visible_sprites, self.obstacle_sprites], 'object', surf)
+        #         if col == 'x':
+        #             Tile((x,y),[self.visible_sprites,self.obstacle_sprites])
+        #         if col == 'p':
+                    
+        self.player = Player((2000,1430),[self.visible_sprites],self.obstacle_sprites)
 
     def run(self):
         #update and draw the game
@@ -44,11 +66,20 @@ class YSortCameraGroup(pygame.sprite.Group):
         self.half_height = self.display_surface.get_size()[1] //2
         self.offset = pygame.math.Vector2()
 
+
+        # Creating the floor
+        self.floor_surf = pygame.image.load('/home/magnetar/Documents/coding/fun/python/Zelda/game/level/graphics/tilemap/ground.png').convert()
+        self.floor_rect = self.floor_surf.get_rect(topleft = (0,0))
+
     def custom_draw(self, player):
 
         #getting the offset
         self.offset.x = player.rect.centerx - self.half_width
         self.offset.y = player.rect.centery - self.half_height
+
+        # Drawing the floor
+        floor_offset_pos = self.floor_rect.topleft - self.offset
+        self.display_surface.blit(self.floor_surf, floor_offset_pos)
         
         # for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key = lambda sprite: sprite.rect.centery):
